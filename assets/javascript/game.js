866bd6a6a561b244a5b8735ad81ea6ec7b304c32
amber phillips <amberphillips@macs-air.attlocal.net>
2018-02-17 22:39:14 -0600

    revisedgame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,29 +2,48 @@
 
  //// Put variables here:       
  var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
- 
  var wins= 0;
  var losses = 0;
  var guessesLeft = 10;
  var guessesFar = 0;
+ 
 
  //// I need to add a button to start the game:
  document.onkeyup = function(event) {
-    var userGuess = event.key;
- }
-/// add something here for the user to guess then what the computer guesses
- var computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
- 
- console.log(computerChoice)
-
- /// add a for loop
+    
+//// add a key for user to use and computer
+var userGuess = String.fromCharCode(event.keyCode);
+var computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+guessesFar.push(userGuess);
 
 
+/// if statements need to be added:
+
+if (userGuess == computerChoice) {
+wins++;
+
+guessesLeft= 10;
+guessesFar.length = 0;
+}
+
+else if (guessesLeft == 0){
+    losses++;
+
+    guessesLeft=10;
+    guessesFar.length = 0;
+}
+else if (userGuess !== computerChoice){
+    guessesLeft--;
+}
+
+var html =
+"<p>You chose: " + userGuess + "</p>" +
+"<p>The computer chose: " + computerGuess + "</p>" +
+"<p>wins: " + wins + "</p>" +
+"<p>losses: " + losses + "</p>" +
+"<p>guessesLeft: " +  + "</p>";
 
 
-
-
-
-
-
+    document.querySelector('#game').innerHTML = html;
+}
 
